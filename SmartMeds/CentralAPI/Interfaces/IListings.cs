5b16b04ccd1258d433c3206aeef30593190dcf17
Zelderon7 <zelderon038@gmail.com>
2025-11-29 23:18:20 +0200

    Listings -> /create

---
+++ SmartMeds/CentralAPI/Interfaces/IListings.cs
@@ -0,0 +1,17 @@
+﻿using SmartMeds.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralAPI.Interfaces
+{
+    public interface IListings
+    {
+        public Task<bool> Create(long hospitalId, double price, DateTime expirationDate, int quantity, long medicineFK);
+        public Task<List<Listing>> GetMyListings(long hospitalId);
+        public Task<List<Listing>> GetOtherListings(long hospitalId);
+        public Task<bool> Delete(long hospitalId, long listingId);
+    }
+}
